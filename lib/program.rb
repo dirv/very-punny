ee2e3c251c0d2e82b671b30be71f8a0a2d02001b
Nick Dyer <nbdyer@gmail.com>
2016-02-05 17:15:56 +0000

    Spike while the iron is hot

--- lib/program.rb
+++ lib/program.rb
@@ -1,8 +1,8 @@
-require 'very_punny'
-
-class Program
+require 'very_punny' 
+class Program 
   DICTIONARY = ["skim", "jim", "grim", "goose", "slim"]
-  PHRASES =  [ "Go out on a limb", "Grim Reaper", "Fatboy Slim", "Carlos Slim", "Gym Class Heroes", "Grim Fandango", "Slim Jim", "Soulja Slim", "Sim City"]
+  PHRASES =  [ "Go out on a limb", "Grim Reaper", "Fatboy Slim", "Carlos Slim", "Gym Class Heroes", "Grim Fandango", "Slim Jim", "Soulja Slim", "Sim City"] 
+  DVD_LIST_PATH = "1001_movie_list.csv"
 
   def initialize(input, output)
     @input = input
@@ -12,8 +12,9 @@ class Program
   def run
     @output.print "Please enter a word: "
 
+    @dictionary = File.open('/usr/share/dict/words', 'r').readlines.map(&:strip).map(&:downcase)
     word = @input.gets.chomp
-    puns = Generator.new(DICTIONARY, PHRASES).generate(word)
+    puns = Generator.new(@dictionary, PHRASES, DVD_LIST_PATH).generate(word)
     puns.each do |pun|
       @output.puts pun
     end
--- lib/very_punny.rb
+++ lib/very_punny.rb
@@ -1,16 +1,21 @@
 require 'httparty'
 require 'rhymer'
+require 'csv'
 
 class Generator
 
-  def initialize(dictionary, phrases)
+  def initialize(dictionary, phrases, dvd_list_path)
     @dictionary = dictionary
     @rhymer = Rhymer.new(dictionary)
     @phrases = phrases
+
+    CSV.foreach(dvd_list_path) do |row|
+      @phrases << row[0]
+    end
   end
 
   def generate(word)
-    raise "Unknown word" unless @dictionary.include?(word)
+    raise "Unknown word" unless @dictionary.include?(word.downcase)
     @rhymer.find(word).map do |rhyme|
       @phrases.select do |phrase|
         words = phrase.downcase.split(' ')
